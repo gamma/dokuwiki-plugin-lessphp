cdb8ac4896fdabc6666d7419a75cfe8203124860
Gerry Weißbach <gerry.w@gammaproduction.de>
2014-12-19 15:19:22 +0100

    Initial commit. lessphp will be a submodule

---
+++ action.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * lessphp wrapper plugin
+ * 
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     i-net software <tools@inetsoftware.de>
+ * @author     Gerry Weissbach <gweissbach@inetsoftware.de>
+ */
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'action.php');
+
+class action_plugin_lessphp extends DokuWiki_Action_Plugin {
+	
+    /**
+	* Register Plugin in DW
+	**/
+    public function register(Doku_Event_Handler $controller) {
+		$controller->register_hook('INIT_LANG_LOAD', 'BEFORE', $this, 'lessphp_load');
+	}
+
+	/**
+	* Check for Template changes
+	**/
+	function lessphp_load(&$event)
+	{
+		global $conf, $INFO;
+        if ( ! strrpos( $_SERVER['SCRIPT_FILENAME'], 'css.php', -7 ) ) {
+            return;
+        }
+		
+		// If this is a call for css.php include the other library
+		require_once(dirname(__FILE__) . '/inc/lessc.inc.php');
+	}
+}
